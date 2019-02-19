f8ec9423f7c854b69c1160a78f3eec2956d27c11
chicavum <47661138+chicavum@users.noreply.github.com>
2019-02-19 14:57:57 +0100

    Add files via upload
    classes employee

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee{
+  public static final String Nombre_DEF="";
+  public static final String Apellidos="";
+  public static final Date Fecha_nac_DEF="";
+  public static final String Nacionalida_DEF="";
+  public stat
+  String Nombre_DEF="";
+  String Apellidos="";
+  Date Fecha_nac_DEF="";
+}
